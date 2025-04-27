658e48b7ae95656326a5705e94b7c74079a9ef77
comic16 <cinemic179@gmail.com>
2025-04-27 09:45:00 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,36 +1,53 @@
 let currentPage = 1;
 const pages = document.querySelectorAll('.page');
+let touchStartX = 0;
+let touchEndX = 0;
+let touchStartY = 0;
+let touchEndY = 0;
 
 function goToPage(pageNum) {
   pages.forEach((page, index) => {
     if (index + 1 === pageNum) {
-      page.style.transform = `translateY(0)`;
+      page.style.transform = `translateX(0)`; // Tampilkan halaman yang sesuai
     } else {
-      page.style.transform = `translateY(100%)`;
+      page.style.transform = `translateX(100%)`; // Sembunyikan halaman lainnya
     }
   });
   currentPage = pageNum;
 }
 
 function swipeEffect() {
-  let touchStartY = 0;
-  let touchEndY = 0;
-
   document.addEventListener('touchstart', (e) => {
+    touchStartX = e.changedTouches[0].screenX;
     touchStartY = e.changedTouches[0].screenY;
   });
 
   document.addEventListener('touchend', (e) => {
+    touchEndX = e.changedTouches[0].screenX;
     touchEndY = e.changedTouches[0].screenY;
-    if (touchStartY > touchEndY + 50) {
-      // Swipe up to go to next page
-      if (currentPage < 5) {
-        goToPage(currentPage + 1);
+
+    // Geser kiri/kanan
+    if (Math.abs(touchStartX - touchEndX) > 50) {
+      if (touchStartX - touchEndX > 50) {
+        // Swipe ke kiri (halaman berikutnya)
+        if (currentPage < 5) {
+          goToPage(currentPage + 1);
+        }
+      } else if (touchEndX - touchStartX > 50) {
+        // Swipe ke kanan (halaman sebelumnya)
+        if (currentPage > 1) {
+          goToPage(currentPage - 1);
+        }
       }
-    } else if (touchStartY < touchEndY - 50) {
-      // Swipe down to go to previous page
-      if (currentPage > 1) {
-        goToPage(currentPage - 1);
+    }
+
+    // Geser atas/bawah untuk kembali menurunkan halaman 2
+    if (Math.abs(touchStartY - touchEndY) > 50) {
+      if (touchStartY < touchEndY) {
+        // Sentuh halaman pertama saat halaman 2 terbuka
+        if (currentPage === 2) {
+          goToPage(1); // Kembali ke halaman 1
+        }
       }
     }
   });
